0553e1b487b7dccb9424e325e6596224b3cdabb5
kacper <kacper.mazur888@gmail.com>
2020-02-18 08:41:01 +0000

    UI Panels & Manager

---
+++ Synthetic Dungeon/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI
+{
+    public class UIManager : MonoBehaviour, IInitializable
+    {
+        private List<UIPanel> _uiPanels = new List<UIPanel>();
+
+        public void Initialize()
+        {
+            AddListeners();
+        }
+
+        public void OpenPanel(UIPanel panel)
+        {
+            foreach (var uiPanel in _uiPanels)
+            {
+                uiPanel.Close();
+            }
+            panel.Open();
+        }
+
+        private void OnDestroy()
+        {
+            RemoveListeners();
+        }
+
+        private void AddListeners()
+        {
+            
+        }
+        
+        private void RemoveListeners()
+        {
+            
+        }
+    }
+}
---
+++ Synthetic Dungeon/Assets/Scripts/UI/UIPanel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI
+{
+    public abstract class UIPanel : MonoBehaviour, IInitializable
+    {
+        enum UiState
+        {
+            NONE,
+            OPEN,
+            CLOSE
+        }
+        private UiState _state;
+        
+        public void Initialize()
+        {
+            _state = UiState.NONE;
+        }
+
+        public virtual void Open()
+        {
+            if (_state == UiState.CLOSE || _state == UiState.NONE)
+            {
+                _state = UiState.OPEN;
+                gameObject.SetActive(true);
+            }
+        }
+        
+        public virtual void Close()
+        {
+            if (_state == UiState.OPEN || _state == UiState.NONE)
+            {
+                _state = UiState.CLOSE;
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
