e2900c314c1b9dceae0e9ef79c3f2100fe2a1a58
kacper <kacper.mazur888@gmail.com>
2020-02-18 09:14:48 +0000

    entity core data

---
+++ Synthetic Dungeon/Assets/Scripts/Entity Core/StatsData.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Entities;
+
+namespace Entity_Core
+{
+    [GenerateAuthoringComponent]
+    public struct StatsData : IComponentData
+    {
+        public int Health;
+        public int Mana;
+        public int Toughness;
+        public int AttackPower;
+        public int MagicPower;
+    }
+}
