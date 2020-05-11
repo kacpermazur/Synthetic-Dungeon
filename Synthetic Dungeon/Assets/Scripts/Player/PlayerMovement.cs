ff5f863fb4f89e71e6b7ff75d7c7f3096abcf2b6
Kacper <kacper.mazur888@gmail.com>
2020-05-12 00:20:12 +0100

    player movement& attack animations

--- Synthetic Dungeon/Assets/Scripts/Player/PlayerMovement.cs
+++ Synthetic Dungeon/Assets/Scripts/Player/PlayerMovement.cs
@@ -8,14 +8,23 @@ namespace Player
     public class PlayerMovement : MonoBehaviour, IInitializable, IOnExecute
     {
         [SerializeField] private float _playerRotationOffset = 90f;
-        
+
         private PlayerData _playerData;
         private Vector2 _onMoveDir;
+        
+        private Animator _animator;
 
         public bool Initialize()
         {
+            _animator = GetComponent<Animator>();
             _playerData = GameManager.Instance.PlayerManager.PlayerData;
-            return true;
+
+            if (_animator)
+            {
+                return true;
+            }
+            
+            return false;
         }
 
         public void OnExecute()
@@ -27,15 +36,16 @@ namespace Player
         {
             Vector3 direction = new Vector3(_onMoveDir.y, 0, -_onMoveDir.x);
             
-
+            _animator.SetFloat("vertical", 0f, 0.1f, Time.deltaTime);
+            
             if (direction != Vector3.zero)
             {
-
                 transform.rotation = Quaternion.Slerp(transform.rotation,  Quaternion.LookRotation(direction),
                     Time.deltaTime * _playerData.rotationSpeed);
                 
                 
                 transform.position += _playerData.movementSpeed * Time.deltaTime * transform.forward;
+                _animator.SetFloat("vertical", 1f);
             }
         }
 
@@ -46,6 +56,7 @@ namespace Player
         
         public void OnAttack(InputValue value)
         {
+            _animator.CrossFade("oh_magic_1", 0.2f);
             GameManager.Instance.PlayerManager.SpellSystem.CastSpell();
         }
     }
--- Synthetic Dungeon/Assets/Scripts/Utilities/AnimationTester.cs
+++ Synthetic Dungeon/Assets/Scripts/Utilities/AnimationTester.cs
@@ -7,6 +7,10 @@ namespace Utilities
     public class AnimationTester : MonoBehaviour
     {
         [SerializeField] [Range(0, 1)] private float speed;
+        [SerializeField] [Range(0, 1)] private float transitionDuration;
+        
+        [SerializeField] private string animName;
+        [SerializeField] private bool play;
         
         private Animator _animator;
         void Start()
@@ -16,7 +20,14 @@ namespace Utilities
 
         void Update()
         {
-            _animator.SetFloat("vertical",speed);
+            if (play)
+            {
+                speed = 0;
+                _animator.CrossFade(animName, transitionDuration);
+                play = false;
+            }
+
+            _animator.SetFloat("vertical", speed);
         }
     }
 }
