dd5e35b115c436ec40da2326f18a9d4d7da22897
Kacper <kacper.mazur888@gmail.com>
2020-05-11 22:21:46 +0100

    animation controller humanoid

---
+++ Synthetic Dungeon/Assets/Scripts/Utilities/AnimationTester.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utilities
+{
+    public class AnimationTester : MonoBehaviour
+    {
+        [SerializeField] [Range(0, 1)] private float speed;
+        
+        private Animator _animator;
+        void Start()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            _animator.SetFloat("vertical",speed);
+        }
+    }
+}
