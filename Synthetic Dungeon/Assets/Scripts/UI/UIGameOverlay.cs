680216f0b476938e7b8215abfe38947e9a2ac675
Kacper <kacper.mazur888@gmail.com>
2020-05-12 14:26:42 +0100

    UI canvas overlay

---
+++ Synthetic Dungeon/Assets/Scripts/UI/UIGameOverlay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class UIGameOverlay : UIPanel
+    {
+        [SerializeField] private Slider healthSlider;
+        [SerializeField] private Slider manaSlider;
+        
+        
+    }
+}
