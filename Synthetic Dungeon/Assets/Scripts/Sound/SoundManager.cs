0682a39961401db4749ede7fac2358c0b056e920
Kacper <kacper.mazur888@gmail.com>
2020-05-26 04:32:58 +0100

    play sound

--- Synthetic Dungeon/Assets/Scripts/Sound/SoundManager.cs
+++ Synthetic Dungeon/Assets/Scripts/Sound/SoundManager.cs
@@ -1,14 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 namespace Sound
 {
     public class SoundManager : MonoBehaviour, IInitializable
     {
+        [SerializeField] private AudioMixer _audioMixer;
+
         public bool Initialize()
         {
+            DontDestroyOnLoad(this);
             return true;
+        }
+
+        public void PlaySound(string name)
+        {
+            
+        }
+
+        public void PlaySoundSpatial(string name)
+        {
+            
         }
     }
 }
