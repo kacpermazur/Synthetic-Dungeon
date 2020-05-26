a1fa3311063741a0486783aeae6b167204a4a6af
Kacper <kacper.mazur888@gmail.com>
2020-05-26 04:43:53 +0100

    sound manager apply clip settings

--- Synthetic Dungeon/Assets/Scripts/Sound/SoundManager.cs
+++ Synthetic Dungeon/Assets/Scripts/Sound/SoundManager.cs
@@ -8,6 +8,9 @@ namespace Sound
     public class SoundManager : MonoBehaviour, IInitializable
     {
         [SerializeField] private AudioMixer _audioMixer;
+
+        private AudioSource _audioSourceMusic;
+        private AudioSource _audioSourceSFX;
         
         public enum SoundType
         {
@@ -19,17 +22,42 @@ namespace Sound
         public bool Initialize()
         {
             DontDestroyOnLoad(this);
-            return true;
+
+            if (_audioMixer != null)
+            {
+                _audioSourceMusic = gameObject.AddComponent<AudioSource>();
+                _audioSourceSFX = gameObject.AddComponent<AudioSource>();
+
+                _audioSourceMusic.outputAudioMixerGroup = _audioMixer.FindMatchingGroups("Music")[0];
+                _audioSourceSFX.outputAudioMixerGroup = _audioMixer.FindMatchingGroups("SFX")[0];
+                
+                return true;
+            }
+            
+            return false;
         }
 
-        public void PlaySound(string name)
+        public void PlaySound(string name, SoundType type)
         {
             
         }
 
-        public void PlaySoundSpatial(string name)
+        public void PlaySoundSpatial(string name, SoundType type)
         {
             
+        }
+
+        public void StopSound(string name, SoundType type)
+        {
+            
+        }
+
+        private void SetAudioClipSettings(ref AudioSource source, SoundClip clip)
+        {
+            source.clip = clip.clip;
+            source.loop = clip.loop;
+            source.volume = clip.volume;
+            source.spatialBlend = clip.spacialBlend;
         }
     }
 }
