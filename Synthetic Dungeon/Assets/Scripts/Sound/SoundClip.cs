a64e5db8a3d5ddc25c1c6e244d95eebcba491ae4
Kacper <kacper.mazur888@gmail.com>
2020-05-26 04:37:00 +0100

    sound clip settings

--- Synthetic Dungeon/Assets/Scripts/Sound/SoundClip.cs
+++ Synthetic Dungeon/Assets/Scripts/Sound/SoundClip.cs
@@ -5,6 +5,13 @@ namespace Sound
     [System.Serializable]
     public class SoundClip
     {
+        public string name;
+        public AudioClip clip;
+        public bool loop;
 
+        [Range(0.0f, 1.0f)] public float volume;
+        [Range(0.0f, 1.0f)] public float spacialBlend;
+
+        [HideInInspector] public AudioSource source;
     }
 }
